dddaa54d1ee544591e30416a8ebe081d6741d085
Theophile-Kango <fadhili.kango@gmail.com>
2020-08-14 07:22:11 +0200

    Add cors and session_store files for configurations

---
+++ config/initializers/cors.rb
@@ -0,0 +1,12 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origin "htpp://localhost:3000"
+        resource "*", headers: :any, method: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+    # allow do
+    #     origin "htpps://authentication-app-react.herokuapp.com"
+    #     resource "*", headers: :any, method: [:get, :post, :put, :patch, :delete, :options, :head],
+    #     credentials: true
+    # end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: "authentication_app", domaine: "authentication-app-api.heroku.com"
