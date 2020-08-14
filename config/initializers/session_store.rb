6a4fd24f2876de04cf7e58270e4fa917adf2d09c
Theophile-Kango <fadhili.kango@gmail.com>
2020-08-14 08:49:45 +0200

    Add production mode in session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "authentication_app", domaine: "authentication-app-api.heroku.com"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "authentication_app", domaine: "authentication-app-api.heroku.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "authentication_app"
+end
